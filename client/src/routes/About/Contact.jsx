993659bba3b2dabdbb4d7fb6d23ac0e62303b179
kinchanshy <kinchanqt@gmail.com>
2023-11-15 20:20:21 +0800

    test

--- client/src/routes/About/Contact.jsx
+++ client/src/routes/About/Contact.jsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { Button, Text } from "@mantine/core";
+import { Image, Text, Box, Divider } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
+import { texts } from "./links";
+import pic from "./Images/UnivProfile.png";
 
-function Contact() {
+function Contacts({ selectedLink }) {
   const navigate = useNavigate();
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100vh",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Contact</Text>
-      <Button
-        onClick={() => {
-          navigate("/");
+    <Box style={{ fontFamily: "Open Sans, sans serif" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "start",
         }}
       >
-        Back
-      </Button>
-    </div>
+        <Text size="1.5rem" fw="bold" p="xs">
+          {selectedLink}
+        </Text>
+      </div>
+      <Divider c="#eeee" size="md" />
+      <div>
+        <Image maw={1000} src={pic} p="md" />
+        
+          <Text  p="xs" ta="justify">
+          We highly prioritize transparent communication with our students, parents, and the broader community. If you have any inquiries, 
+issues, or would like to gain further insight into our school, please feel free to contact us. You can easily reach out to us by utilizing 
+the contact details supplied below.
+          </Text>
+      
+      </div>
+    </Box>
   );
 }
 
-export default Contact;
+export default Contacts;
