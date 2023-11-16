eba25add6164185912eeeae3a405d820749eed15
kinchanshy <kinchanqt@gmail.com>
2023-11-16 14:42:47 +0800

    put the citizens charter link from the homepage

--- client/src/Components/WebView/WebView.jsx
+++ client/src/Components/WebView/WebView.jsx
@@ -347,14 +347,28 @@ function WebView() {
               {/* Logo of the seal */}
               <Image maw={250} src={seal} p="md" />
               {/* Text about the seal */}
-              <Text fz="md" p="md" c="#fff" ta="justify">
+              <Text fz="lg" p="md" c="#fff" ta="justify">
                 PLM is compliant with the Anti-Red Tape Act (ARTA), as validated
                 by the Civil Service Commission-Malacañan Palace. Pursuant to
                 the Philippine Government’s Transparency Principle, our
                 Transparency Coordinating Council, created through
-                Administrative Order No. 2013-12. Read our Citizen's Charter
-                here. In case of complaints, please submit proof or any
-                supporting documents through this link.
+                Administrative Order No. 2013-12. Read our {""}
+                <Text
+                  span
+                  fw="bold"
+                  onClick={() => {
+                    navigate("/citizens-charter");
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  Citizen's Charter here.
+                </Text>
+                {""} In case of complaints, please submit proof or any
+                supporting documents through this {""}
+                <Text span fw="bold" style={{ cursor: "pointer" }}>
+                  link
+                </Text>
+                .
               </Text>
             </Container>
           </div>
